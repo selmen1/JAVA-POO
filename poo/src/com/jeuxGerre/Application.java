83ba632af44d39c571fbcd9cab9795406083a314
selmen1 <selmen.harhad@gmail.com>
2019-02-12 13:36:52 +0100

    test Application .

--- poo/src/com/jeuxGerre/Application.java
+++ poo/src/com/jeuxGerre/Application.java
@@ -1,4 +1,14 @@
 package com.jeuxGerre;
 
 public class Application {
+    public static void main(String[] args) {
+        Personnage[] tPers = {new Guerrier(), new Chirurgien(), new Civil(), new Sniper(), new Medecin()};
+
+        for(Personnage p : tPers){
+            System.out.println("\nInstance de " + p.getClass().getName());
+            System.out.println("***************************************");
+            p.combattre();
+            p.seDeplacer();
+        }
+    }
 }
