907db9d1d42e580a673aa8d66803bbb3955a40fc
selmen1 <selmen.harhad@gmail.com>
2019-02-14 14:16:31 +0100

    nouvel package comportement. 3 interface à implementé.

---
+++ poo/src/com/comportement/Deplacement.java
@@ -0,0 +1,5 @@
+package com.comportement;
+
+public interface Deplacement {
+    public void deplacer();
+}
---
+++ poo/src/com/comportement/EspritCombatif.java
@@ -0,0 +1,5 @@
+package com.comportement;
+
+public interface EspritCombatif {
+    public void combat();
+}
